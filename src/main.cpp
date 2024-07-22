ab0894b3619699a37a53fb5d4a46109ad45442e7
alon <alon@cariboulabs.co>
2024-07-22 18:55:28 +0300

    first

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,24 @@
 #include <string>
 #include <algorithm>
 #include <IMU_Preintegrator.h>
-#include<fstream>
+#include <fstream>
 
-#include<opencv2/core/core.hpp>
+#include <opencv2/core/core.hpp>
 
 
 void LoadEuRocIMUData(const std::string &strImuPath, std::vector<IMUData> &imu_datas);
+void LoadSrulikData(const std::string &strImuPath, std::vector<IMUData> &imu_datas);
+Eigen::Vector4d RotationMatrixToQuat(const Eigen::Matrix3d& q);
 
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+    std::ofstream output("out.csv");
+    std::string imu_file_path("/home/alon/repos/IMU_Preintegration/data/data.csv");
 
     std::vector<IMUData> imu_datas;
-
-    std::string imu_file_path("/home/ujasmandavia/Desktop/IMU_Preintegration/data/data.csv");
     LoadEuRocIMUData(imu_file_path, imu_datas);
 
     IMUPreintegrator IMUPreintegrator;
-
-    std::ofstream output("/home/ujasmandavia/Desktop/IMU_Preintegration/data/new_pre.txt");
-
-    for(int i=0; i<20; ++i)
+    for(int i=0; i < imu_datas.size(); ++i)
     {
         IMUData imu_data = imu_datas[i];
         double dt = imu_datas[i+1].timestamp_ - imu_data.timestamp_;
@@ -31,34 +28,36 @@ int main(int argc, char **argv)
         IMUPreintegrator.Update(imu_data.gyroscope_, imu_data.accelerometer_, dt);
 
 
-        output << "Pre P " << i << std::endl;
-        output << IMUPreintegrator.GetDeltaP() << std::endl;
-        output << "Pre V " << i << std::endl;
-        output << IMUPreintegrator.GetDeltaV() << std::endl;
-        output << "Pre R " << i << std::endl;
-        output << IMUPreintegrator.GetDeltaRot() << std::endl;
-        output << "Pre Cov " << i << std::endl;
-        output << IMUPreintegrator.GetCovPVRot() << std::endl;
+        auto delta_rot_R = IMUPreintegrator.GetDeltaRot();
+        auto delta_rot_quat = RotationMatrixToQuat(delta_rot_R);
+        output << delta_rot_quat[0] << "," << delta_rot_quat[1] << "," << delta_rot_quat[2] << "," << delta_rot_quat[3] << std::endl;
+        auto delta_p = IMUPreintegrator.GetDeltaP();
+        output << delta_p[0] << "," << delta_p[1] << "," << delta_p[2] << "," << std::endl;
+        auto delta_v = IMUPreintegrator.GetDeltaV();
+        output << delta_v[0] << "," << delta_v[1] << "," << delta_v[2] << "," << std::endl;
+        
 
-        output << "Pre P jacobian gyr " << i << std::endl;
-        output << IMUPreintegrator.GetPJacoBiasgur() << std::endl;
-        output << "Pre P jacobian acc " << i << std::endl;
-        output << IMUPreintegrator.GetPJacoBiasacc() << std::endl;
-        output << "Pre V jacobian gyr " << i << std::endl;
-        output << IMUPreintegrator.GetVJacobBiasgyro() << std::endl;
-        output << "Pre V jacobian acc " << i << std::endl;
-        output << IMUPreintegrator.GetVJacobBiasacc() << std::endl;
-        output << "Pre Rot jacobian acc " << i << std::endl;
-        output << IMUPreintegrator.GetRotJacobBiasgyr() << std::endl;
+        // output << IMUPreintegrator.GetDeltaV() << std::endl;
+        // output << "Pre R " << i << std::endl;
+        // output << IMUPreintegrator.GetDeltaRot() << std::endl;
+        // output << "Pre Cov " << i << std::endl;
+        // output << IMUPreintegrator.GetCovPVRot() << std::endl;
+
+        // output << "Pre P jacobian gyr " << i << std::endl;
+        // output << IMUPreintegrator.GetPJacoBiasgur() << std::endl;
+        // output << "Pre P jacobian acc " << i << std::endl;
+        // output << IMUPreintegrator.GetPJacoBiasacc() << std::endl;
+        // output << "Pre V jacobian gyr " << i << std::endl;
+        // output << IMUPreintegrator.GetVJacobBiasgyro() << std::endl;
+        // output << "Pre V jacobian acc " << i << std::endl;
+        // output << IMUPreintegrator.GetVJacobBiasacc() << std::endl;
+        // output << "Pre Rot jacobian acc " << i << std::endl;
+        // output << IMUPreintegrator.GetRotJacobBiasgyr() << std::endl;
 
     }
-
-
+    printf("finished integration of %d imu data samples\n", imu_datas.size());
     return 0;
-
 }
-
-
 
 void LoadEuRocIMUData(const std::string &strImuPath, std::vector<IMUData> &imu_datas)
 {
@@ -100,4 +99,36 @@ void LoadEuRocIMUData(const std::string &strImuPath, std::vector<IMUData> &imu_d
 
         }
     }
+}
+
+Eigen::Vector4d RotationMatrixToQuat(const Eigen::Matrix3d& q) {
+    Eigen::Vector4d quat;
+    double tr = q.trace();
+
+    if (tr > 0) {
+        double S = sqrt(tr + 1.0) * 2; // S=4*qw
+        quat[0] = 0.25 * S;
+        quat[1] = (q(2,1) - q(1,2)) / S;
+        quat[2] = (q(0,2) - q(2,0)) / S;
+        quat[3] = (q(1,0) - q(0,1)) / S;
+    } else if ((q(0,0) > q(1,1)) && (q(0,0) > q(2,2))) {
+        double S = sqrt(1.0 + q(0,0) - q(1,1) - q(2,2)) * 2; // S=4*qx
+        quat[0] = (q(2,1) - q(1,2)) / S;
+        quat[1] = 0.25 * S;
+        quat[2] = (q(0,1) + q(1,0)) / S;
+        quat[3] = (q(0,2) + q(2,0)) / S;
+    } else if (q(1,1) > q(2,2)) {
+        double S = sqrt(1.0 + q(1,1) - q(0,0) - q(2,2)) * 2; // S=4*qy
+        quat[0] = (q(0,2) - q(2,0)) / S;
+        quat[1] = (q(0,1) + q(1,0)) / S;
+        quat[2] = 0.25 * S;
+        quat[3] = (q(1,2) + q(2,1)) / S;
+    } else {
+        double S = sqrt(1.0 + q(2,2) - q(0,0) - q(1,1)) * 2; // S=4*qz
+        quat[0] = (q(1,0) - q(0,1)) / S;
+        quat[1] = (q(1,2) + q(2,1)) / S;
+        quat[2] = (q(0,1) + q(1,0)) / S;
+        quat[3] = 0.25 * S;
+    }
+    return quat;
 }
